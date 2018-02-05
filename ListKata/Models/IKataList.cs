35e99f7299701704f7ffd73977b21d3ef8cb6610
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-02-05 15:02:15 +0000

    Added List interface and node definition

---
+++ ListKata/Models/IKataList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListKata.Models
+{
+    public interface IKataList : IEquatable<string>
+    {
+        ListNode First { get; set; }
+        void Add(string item);
+        ListNode Find(string item);
+        void Delete(ListNode item);
+    }
+}
---
+++ ListKata/Models/ListNode.cs
@@ -0,0 +1,9 @@
+﻿namespace ListKata.Models
+{
+    public class ListNode
+    {
+        public string Value { get; set; }
+        public ListNode Previous { get; set; }
+        public ListNode Next { get; set; }
+    }
+}
